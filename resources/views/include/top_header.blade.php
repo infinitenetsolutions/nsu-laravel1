a9d9f679e781838e0596b2309396c6ca21ec9c59
raja <raja@raja.com>
2022-04-14 15:44:02 +0530

    all done

---
+++ resources/views/include/top_header.blade.php
@@ -0,0 +1,25 @@
+<section  >
+    <div class="container">
+        <div class="row" style="display: contents;">
+
+            <a href="" style="font-weight: 700;text-align: center;font-size: 14px;">Admission Helpline <br> 0657-223-3022, 9386-817-857 </a><a href="" style="float: right;background: #000;color: #fff;padding: 10px 20px;position: absolute;top: 1px;right: 0px;font-weight: 600;">Apply Online</a>
+
+        </div>
+    </div>
+</section>
+<style>
+    @media(max-width:576px) {
+        .mobile_nav .mobile_nav_icon {
+            top: 55px !important;
+        }
+
+        .navbar-expand-lg {
+            top: 25px !important;
+        }
+
+        .mobile_nav_icon {
+            top: 50px !important;
+        }
+
+    }
+</style>
