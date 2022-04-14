f819132074dfeccf5533677ed304915ace78e107
raja <raja@raja.com>
2022-04-14 16:28:54 +0530

    al done

--- resources/views/include/top_header.blade.php
+++ resources/views/include/top_header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row" style="display: contents;">
 
-            <a href="" style="font-weight: 700;text-align: center;font-size: 14px;">Admission Helpline <br> 0657-223-3022, 9386-817-857 </a><a href="" style="float: right;background: #000;color: #fff;padding: 10px 20px;position: absolute;top: 1px;right: 0px;font-weight: 600;">Apply Online</a>
+            <a href="https://nsucms.in/prospectus/public/" style="font-weight: 700;text-align: center;font-size: 14px;">Admission Helpline <br> 0657-223-3022, 9386-817-857 </a><a href="" style="float: right;background: #000;color: #fff;padding: 10px 20px;position: absolute;top: 1px;right: 0px;font-weight: 600;">Apply Online</a>
 
         </div>
     </div>
