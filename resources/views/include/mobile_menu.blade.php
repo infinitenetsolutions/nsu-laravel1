966c6ae6ea23faef125224b24081595267c8a634
raja <raja@raja.com>
2022-04-15 15:53:34 +0530

    all doen

--- resources/views/include/mobile_menu.blade.php
+++ resources/views/include/mobile_menu.blade.php
@@ -10,7 +10,7 @@
                 ->get();
             ?>
             @foreach ($admissions as $admission)
-            <li><a href="{{ route('admission', $admission->title) }}"> {{ $admission->sub_title }}</a></li>
+                <li><a href="{{ route('admission', $admission->title) }}"> {{ $admission->sub_title }}</a></li>
             @endforeach
             <li><a href="{{ route('course_list') }}">Admission Enquiry</a></li>
 
@@ -29,8 +29,8 @@
         ?>
         <ul class="dropdown-menu">
             @foreach ($abouts as $about)
-            <li><a href="{{ route('about', $about->title) }}">
-                    {{ $about->sub_title }}</a></li>
+                <li><a href="{{ route('about', $about->title) }}">
+                        {{ $about->sub_title }}</a></li>
             @endforeach
 
             <?php $facultyes = DB::table('faculty_tbl')
@@ -39,8 +39,8 @@
                 ->get(['type']);
             ?>
             @foreach ($facultyes as $faculty)
-            <li><a href="{{ route('faculty', $faculty->type) }}">Meet
-                    our {{ $faculty->type }}</a></li>
+                <li><a href="{{ route('faculty', $faculty->type) }}">Meet
+                        our {{ $faculty->type }}</a></li>
             @endforeach
             <?php $studentpdfs = DB::table('pdf')
                 ->where('type', 'aboutpdf')
@@ -49,12 +49,11 @@
             ?>
             <li><a href="{{ route('govbody') }}"> The Governing Body</a></li>
             @foreach ($studentpdfs as $studentpdf)
-            @if (url_check($home_url . 'pdf/' . $studentpdf->images))
-
-            <li><a target="_blank" href="{{ $home_url . 'pdf/' . $studentpdf->images }}">
-                    {{ $studentpdf->title }}</a>
-            </li>
-            @endif
+                @if (url_check($home_url . 'pdf/' . $studentpdf->images))
+                    <li><a target="_blank" href="{{ $home_url . 'pdf/' . $studentpdf->images }}">
+                            {{ $studentpdf->title }}</a>
+                    </li>
+                @endif
             @endforeach
         </ul>
     </li>
@@ -72,9 +71,10 @@
 
         <ul class="dropdown-menu">
             @foreach ($programs as $program)
-            <li> <a class="color-orange text-capitalize" href="{{ route('program', strtolower(str_replace(' ', '-', $program->program))) }}"><b>
-                        <?php echo $program->program; ?>
-                    </b></a> </li>
+                <li> <a class="color-orange text-capitalize"
+                        href="{{ route('program', strtolower(str_replace(' ', '-', $program->program))) }}"><b>
+                            <?php echo $program->program; ?>
+                        </b></a> </li>
             @endforeach
         </ul>
     </li>
@@ -88,8 +88,8 @@
         ?>
         <ul class="dropdown-menu">
             @foreach ($infrastructures as $infrastructure)
-            <li><a href="{{ route('infrastructure', $infrastructure->title) }}">
-                    {{ $infrastructure->sub_title }}</a></li>
+                <li><a href="{{ route('infrastructure', $infrastructure->title) }}">
+                        {{ $infrastructure->sub_title }}</a></li>
             @endforeach
         </ul>
     </li>
@@ -105,8 +105,8 @@
         ?>
         <ul class="dropdown-menu">
             @foreach ($infrastructures as $infrastructure)
-            <li><a href="{{ route('placement', $infrastructure->title) }}">
-                    {{ $infrastructure->sub_title }}</a></li>
+                <li><a href="{{ route('placement', $infrastructure->title) }}">
+                        {{ $infrastructure->sub_title }}</a></li>
             @endforeach
 
 
@@ -127,8 +127,8 @@
         ?>
         <ul class="dropdown-menu">
             @foreach ($infrastructures as $infrastructure)
-            <li><a href="{{ route('student', $infrastructure->title) }}">
-                    {{ $infrastructure->sub_title }}</a></li>
+                <li><a href="{{ route('student', $infrastructure->title) }}">
+                        {{ $infrastructure->sub_title }}</a></li>
             @endforeach
             <?php $studentpdfs = DB::table('pdf')
                 ->where('type', 'studentpdf')
@@ -137,12 +137,12 @@
             ?>
 
             @foreach ($studentpdfs as $studentpdf)
-            @if (url_check($home_url . 'pdf/' . $studentpdf->images))
-            <li> <a target="_blank" href="{{ $home_url . 'pdf/' . $studentpdf->images }}">
+                @if (url_check($home_url . 'pdf/' . $studentpdf->images))
+                    <li> <a target="_blank" href="{{ $home_url . 'pdf/' . $studentpdf->images }}">
 
-                    {{ $studentpdf->title }}
-                </a></li>
-            @endif
+                            {{ $studentpdf->title }}
+                        </a></li>
+                @endif
             @endforeach
 
         </ul>
@@ -158,7 +158,8 @@
         </ul>
     </li>
 
-    <li class="nav-item hidden-xs"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Education Loan</a>
+    <li class="nav-item hidden-xs"><a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
+            aria-expanded="false">Education Loan</a>
         <ul class="dropdown-menu">
             <li><a href="https://propelld.com/signin">Propelld</a></li>
             <li><a href="https://neevfinance.hagnosoft.com/webform">neevfinance</a></li>
@@ -170,19 +171,22 @@
     <li class="nav-item"><a class="nav-link apply_btn" href="https://nsucms.in/prospectus/public/">Apply
             Online</a>
     </li>
-   
 
+<br>
     <ul class="address" style="padding: 5px 0px 0px 15px;">
-        <li class="nsu_map"><i class="fas fa-map"></i> #1. Pokhari,Near Bhilai Pahadi, Jamshedpur,Jharkhand </li>
-        <li class="nsu_map"><i class="fas fa-map"></i> #2. Shatabdi Tower, 4th Floor, Sakchi, Jamshedpur, Jharkhand </li>
-        <li class="text-white" ><i class="fa fa-phone "></i> 0657-223-3022, 9386-817-857
+        <li class="nsu_map"><i class="fas fa-map"></i> #1. Pokhari,Near Bhilai Pahadi, Jamshedpur,Jharkhand
+        </li>
+        <li class="nsu_map"><i class="fas fa-map"></i> #2. Shatabdi Tower, 4th Floor, Sakchi, Jamshedpur,
+            Jharkhand </li>
+        <li class="text-white"><i class="fa fa-phone "></i> 0657-223-3022, 9386-817-857
 
         </li>
-        <li class="text-white" ><i class="fas fa-envelope"></i> info@nsuniv.ac.in
+        <li class="text-white"><i class="fas fa-envelope"></i> info@nsuniv.ac.in
         </li>
 
 
-        <li class="nav-item"><a class="nav-link apply_btn" href="https://nsucms.in/prospectus/public/">Vision 2k22 </a>
+        <li class="nav-item"><a class="nav-link apply_btn" href="https://nsucms.in/prospectus/public/">Vision 2k22
+            </a>
         </li>
 
 
@@ -205,4 +209,5 @@
     .slimScrollDiv {
         overflow: scroll !important;
     }
+
 </style>
