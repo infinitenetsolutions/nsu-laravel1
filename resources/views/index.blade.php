f183ac33c5a99a2fe36907717ae09b24b81e4b10
raja <raja@raja.com>
2022-04-19 13:38:53 +0530

    all done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,8 +72,8 @@
             <div class="container">
                 <div class="row ">
                     <div class="col-md-4">
-                        <h2 class="wow fadeInDown mb-1 text-uppercase">
-                            <span class="border-bottom mb-4 text-dark">Notice</span> <span
+                        <h2 class="wow fadeInDown mb-3 text-uppercase">
+                            <span class="border-bottom mb-2 text-dark">Notice</span> <span
                                 class="text-white ">Board</span>
                         </h2>
                         <div class="double-line-bottom-theme-colored-2"></div>
@@ -114,8 +114,8 @@
                     </div>
 
                     <div class="col-12 col-md-8 col-lg-8 col-xl-8">
-                        <h2 class="wow mb-1 fadeInDown text-uppercase">
-                            <span class="border-bottom mb-4 text-bolder text-dark">University</span> <span
+                        <h2 class="wow mb-3 fadeInDown text-uppercase">
+                            <span class="border-bottom mb-2 text-bolder text-dark">University</span> <span
                                 class="text-white ">Tour</span>
                         </h2>
                         <div class="double-line-bottom-theme-colored-2"></div>
