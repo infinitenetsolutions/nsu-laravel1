0fa36a184e489aefbd8bf9c60bc49f41f7f302e7
raja <raja@raja.com>
2022-04-20 18:03:09 +0530

    all done

--- resources/views/career.blade.php
+++ resources/views/career.blade.php
@@ -170,7 +170,7 @@
                                                 <div class="accordion-inner">
 
                                                     <div class="row">
-                                                        <div class=" font-13">
+                                                        <div class=" font-13  ml-5 pl-5">
                                                             Start Date:
                                                             {{ $career->start }}
                                                         </div>
